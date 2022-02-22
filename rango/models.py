7599d3fb70931545e51b84d0420a0e4897a94a42
Xinyao Shu <2491689s@student.gla.ac.uk>
2022-02-22 18:30:23 +0000

    chap5 bugfixed, test passed

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 
 	class Meta:
 		verbose_name_plural = 'Categories'
@@ -14,6 +16,7 @@ class Page(models.Model):
 	title = models.CharField(max_length=128)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
+
 
 	def __str__(self):
 		return self.title
