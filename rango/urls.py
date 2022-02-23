e84a6ec103dcdad410c6d1e7b02529297d27ee57
Xinyao Shu <2491689s@student.gla.ac.uk>
2022-02-23 20:39:23 +0000

    chap9 test passed

--- rango/urls.py
+++ rango/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
          views.show_category, name='show_category'),
     path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('add_category/', views.add_category, name='add_category'),
-    
-
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('restricted/', views.restricted, name='restricted'),
 ]
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -94,6 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        #'OPTIONS': {'min_length': 6,},
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -103,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+LOGIN_URL = 'rango:login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
